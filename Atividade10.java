cc9b8d98b19367936e336acd340b2bce87fbd6e5
pedrocorrea2002 <63562843+pedrocorrea2002@users.noreply.github.com>
2020-05-05 15:46:49 -0300

    Update Atividade10.java

--- Atividade10.java
+++ Atividade10.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class Décimo{
+public class Atividade10{
 	public static void main(String[] args){
 	
 	double altura=0;
@@ -8,33 +8,33 @@ public class Décimo{
 	
 	Scanner numb = new Scanner(System.in);
 	
-	System.out.println("Digite sua altura em centimetros");
+	System.out.println("Digite sua altura em centÃ­metros");
 	{while(altura == 0){	
 		try{
 		 altura = Integer.parseInt(numb.nextLine());
 		} catch (NumberFormatException e){
-			System.out.println("Não entendi, repita por favor");
+			System.out.println("NÃ£o entendi, repita por favor");
 		}}
 	
-	System.out.println("Você é Homem ou Mulher?");
+	System.out.println("VocÃª Ã© Homem ou Mulher?");
 	System.out.println("a)Homem");
 	System.out.println("b)Mulher");
 	
 	opcao = numb.next().trim();
 	
 	if ((opcao.equalsIgnoreCase("a"))|(opcao.equalsIgnoreCase("b"))){
-		if (opcao=="a"){
+		if ((opcao.equalsIgnoreCase("a"))){
 			System.out.println("O seu peso ideal seria "+(((altura*(72.7/100))-58))+" KGs");
 	}
-		else{
+		if ((opcao.equalsIgnoreCase("b"))){
 			System.out.println("O seu peso ideal seria "+(((altura*(62.1/100))-(44.7)))+" KGs");
 		}
 		}
 	else{
 	while(!((opcao.equalsIgnoreCase("a"))|(opcao.equalsIgnoreCase("b")))){
 		
-		System.out.println("OPÇÃO INCORRETA!");
-		System.out.println("Você é Homem ou Mulher?");
+		System.out.println("OPÃ‡ÃƒO INCORRETA!");
+		System.out.println("VocÃª Ã© Homem ou Mulher?");
 		System.out.println("a)Homem");
 		System.out.println("b)Mulher");
 		
@@ -43,7 +43,7 @@ public class Décimo{
 	if ((opcao.equalsIgnoreCase("a"))){
 		System.out.println("O seu peso ideal seria "+(((altura*(72.7/100))-58))+" KGs");
 }
-	else{
+	if ((opcao.equalsIgnoreCase("b"))){
 		System.out.println("O seu peso ideal seria "+(((altura*(62.1/100))-(44.7)))+" KGs");
 	}}}}}
 	
